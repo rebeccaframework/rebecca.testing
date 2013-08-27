789729eaea0215648fb774d3e66a15c5f9e7032c
Atsushi Odagiri <aodagx@gmail.com>
2013-08-28 00:14:49 +0900

    added test

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,4 +1,21 @@
+from pyramid import testing
 from rebecca.testing import config
 
+
+class DummyView(object):
+    def __init__(self, request):
+        self.request = request
+
+    def __call__(self):
+        from pyramid.httpexceptions import HTTPFound
+        return HTTPFound(self.request.route_url("testing"))
+
+
 def test_it(config):
-    pass
+    config.add_route("testing", "/this/is/testing")
+
+    request = testing.DummyRequest()
+    view = DummyView(request)
+    res = view()
+
+    assert res.location == 'http://example.com/this/is/testing'
